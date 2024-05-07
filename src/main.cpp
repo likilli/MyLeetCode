4b5168161206b406b48e6189e3c088802e4a71ef
李凯 <likai@likaideMacBook-Pro.local>
2024-05-08 00:49:07 +0800

    list: Create list, traversal list and reverse list

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <vector>
+
+#include "list.h"
+
+
+using ListNode = ListNodes<int>;
 
 int main()
 {
-    std::cout << "Hello world" << std::endl;
+    const std::vector<int> vec{1, 7, 3, 5, 9, 4, 8};
+    auto list = CreateList(vec);
+
+
+    TraversalList(list);
+
+    auto res = ReverseList(list);
+    TraversalList(res);
+
 
     return 0;
 }
---
+++ src/structure/list.h
@@ -0,0 +1,71 @@
+
+#pragma once
+
+#include <vector>
+#include <iostream>
+
+template <typename DataType>
+struct ListNodes
+{
+    DataType val;
+    ListNodes *next;
+
+    explicit ListNodes(const DataType& value);
+    ListNodes(const DataType& value, ListNodes* next);
+};
+
+template <typename DataType>
+ListNodes<DataType>::ListNodes(const DataType &value) : val(value) {
+    next = nullptr;
+}
+
+template <typename DataType>
+ListNodes<DataType>::ListNodes(const DataType &value, ListNodes *next) : val(value), next(next)
+{}
+
+template <typename DataType>
+ListNodes<DataType>* CreateList(const std::vector<DataType>& vec) {
+    auto *root = new ListNodes<DataType>(0);
+    ListNodes<DataType> *cur = root;
+    for (const auto & e : vec) {
+        cur->next = new ListNodes<DataType>(e);
+        cur = cur->next;
+    }
+
+    auto res = root->next;
+    delete root;
+    root = nullptr;
+
+    return res;
+}
+
+template <typename DataType>
+void TraversalList(ListNodes<DataType>* root)
+{
+    ListNodes<DataType> *cur = root;
+    while (cur)
+    {
+        std::cout << cur->val << ' ';
+        cur = cur->next;
+    }
+    std::cout << std::endl;
+}
+
+template <typename DataType>
+ListNodes<DataType>* ReverseList(ListNodes<DataType>* root)
+{
+    if (!root)
+        return nullptr;
+
+    ListNodes<DataType> *prev = nullptr;
+    ListNodes<DataType> *next = nullptr;
+    while (root)
+    {
+        next = root->next;
+        root->next = prev;
+        prev = root;
+        root = next;
+
+    }
+    return prev;
+}
